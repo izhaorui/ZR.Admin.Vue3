52e0dc630303b889f21e51d179833cc2d9099b14
不做码农 <599854767@qq.com>
2022-04-18 19:57:17 +0800

    fix复制问题

--- src/directive/module/clipboard.js
+++ src/directive/module/clipboard.js
@@ -1,33 +1,34 @@
 /**
-* v-clipboard 文字复制剪贴
-* Copyright (c) 2021 ruoyi
-*/
+ * v-clipboard 文字复制剪贴
+ * Copyright (c) 2021 ruoyi
+ */
 
 import Clipboard from 'clipboard'
 export default {
-  bind(el, binding, vnode) {
+  mounted(el, binding, vnode) {
     switch (binding.arg) {
-      case 'success':
-        el._vClipBoard_success = binding.value;
-        break;
-      case 'error':
-        el._vClipBoard_error = binding.value;
-        break;
-      default: {
-        const clipboard = new Clipboard(el, {
-          text: () => binding.value,
-          action: () => binding.arg === 'cut' ? 'cut' : 'copy'
-        });
-        clipboard.on('success', e => {
-          const callback = el._vClipBoard_success;
-          callback && callback(e);
-        });
-        clipboard.on('error', e => {
-          const callback = el._vClipBoard_error;
-          callback && callback(e);
-        });
-        el._vClipBoard = clipboard;
-      }
+    case 'success':
+      el._vClipBoard_success = binding.value;
+      break;
+    case 'error':
+      el._vClipBoard_error = binding.value;
+      break;
+    default: {
+      const clipboard = new Clipboard(el, {
+        text: () => binding.value,
+        action: () => binding.arg === 'cut' ? 'cut' : 'copy'
+      });
+      clipboard.on('success', e => {
+        const callback = el._vClipBoard_success;
+        callback && callback(e);
+      });
+      clipboard.on('error', e => {
+        console.log('复制失败', e)
+        const callback = el._vClipBoard_error;
+        callback && callback(e);
+      });
+      el._vClipBoard = clipboard;
+    }
     }
   },
   update(el, binding) {
