49a4f25a15f15aa07349453e85aa6b881d198b61
不做码农 <599854767@qq.com>
2022-04-20 16:56:31 +0800

    新增加拖动指令

--- src/directive/index.js
+++ src/directive/index.js
@@ -1,9 +1,11 @@
 import hasRole from './permission/hasRole'
 import hasPermi from './permission/hasPermi'
 import clipboard from './module/clipboard'
+import drag from './module/drag'
 
 export default function directive(app){
   app.directive('hasRole', hasRole)
   app.directive('hasPermi', hasPermi)
 	app.directive('clipboard', clipboard)
+	app.directive('drag', drag)
 }
