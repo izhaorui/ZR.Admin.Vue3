bacc55df0bab196d5c8bd55198eb33e1815a609d
不做码农 <599854767@qq.com>
2022-04-27 11:36:45 +0800

    update login.vue

--- src/views/login.vue
+++ src/views/login.vue
@@ -29,7 +29,7 @@
       </el-form-item>
       <el-checkbox v-model="loginForm.rememberMe" style="margin: 0px 0px 25px 0px">记住密码</el-checkbox>
       <el-form-item style="width: 100%">
-        <el-button :loading="loading" size="large" type="primary" style="width: 100%" @click.prevent="handleLogin">
+        <el-button :loading="loading" size="large" plain type="primary" style="width: 100%" @click.prevent="handleLogin">
           <span v-if="!loading">登 录</span>
           <span v-else>登 录 中...</span>
         </el-button>
