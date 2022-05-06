d8b3c45e3fd5c38af0239076c3200a1c644f378c
不做码农 <599854767@qq.com>
2022-05-06 15:55:19 +0800

    Token存储改用localStorage

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,15 @@
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
 
 const TokenKey = 'ZR-Token'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return localStorage.getItem(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return localStorage.setItem(TokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return localStorage.removeItem(TokenKey)
 }
