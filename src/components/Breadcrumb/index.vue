945f764249762c4e4a533abc43cbc09be06f4c3b
不做码农 <599854767@qq.com>
2022-05-21 11:15:02 +0800

    面包屑移除动画

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,30 +1,31 @@
 <template>
   <el-breadcrumb class="app-breadcrumb" separator="/">
-    <transition-group name="breadcrumb">
-      <el-breadcrumb-item v-for="(item,index) in levelList" :key="item.path">
-        <span v-if="item.redirect === 'noRedirect' || index == levelList.length - 1" class="no-redirect">{{ item.meta.title }}</span>
-        <a v-else @click.prevent="handleLink(item)">{{ item.meta.title }}</a>
-      </el-breadcrumb-item>
-    </transition-group>
+    <!-- <transition-group name="breadcrumb"> -->
+    <el-breadcrumb-item v-for="(item, index) in levelList" :key="item.path">
+      <span v-if="item.redirect === 'noRedirect' || index == levelList.length - 1" class="no-redirect">{{ item.meta.title }}</span>
+      <a v-else @click.prevent="handleLink(item)">{{ item.meta.title }}</a>
+    </el-breadcrumb-item>
+    <!-- </transition-group> -->
   </el-breadcrumb>
 </template>
 
 <script setup>
-const route = useRoute();
-const router = useRouter();
+const route = useRoute()
+const router = useRouter()
 const levelList = ref([])
 
 function getBreadcrumb() {
   // only show routes with meta.title
-  let matched = route.matched.filter(item => item.meta && item.meta.title);
-  const first = matched[0]
+  let matched = route.matched.filter((item) => item.meta && item.meta.title)
+  // const first = matched[0]
   // 判断是否为首页
   // if (!isDashboard(first)) {
   //   matched = [{ path: '/index', meta: { title: '首页' } }].concat(matched)
   // }
 
-  levelList.value = matched.filter(item => item.meta && item.meta.title && item.meta.breadcrumb !== false)
+  levelList.value = matched.filter((item) => item.meta && item.meta.title && item.meta.breadcrumb !== false)
 }
+
 function isDashboard(route) {
   const name = route && route.name
   if (!name) {
@@ -48,10 +49,10 @@ watchEffect(() => {
   }
   getBreadcrumb()
 })
-getBreadcrumb();
+getBreadcrumb()
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .app-breadcrumb.el-breadcrumb {
   display: inline-block;
   font-size: 14px;
@@ -63,7 +64,7 @@ getBreadcrumb();
     cursor: text;
   }
 }
-.mobile .app-breadcrumb.el-breadcrumb{
-	display: none;
+.mobile .app-breadcrumb.el-breadcrumb {
+  display: none;
 }
 </style>
