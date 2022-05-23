623e07128821ce308ac839681883aec4e8996c90
不做码农 <599854767@qq.com>
2022-05-23 18:07:34 +0800

    add AppMain.vue

---
+++ src/layout/components/AppMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-main class="app-main">
+    <router-view v-slot="{ Component, route }" :key="route.path">
+      <transition appear name="fade-transform" mode="out-in">
+        <keep-alive :include="cachedViews">
+          <component :is="Component" :key="route.path" />
+        </keep-alive>
+      </transition>
+    </router-view>
+  </el-main>
+</template>
+<script setup>
+const route = useRoute()
+const store = useStore()
+const cachedViews = computed(() => {
+  return store.state.tagsView.cachedViews
+})
+</script>
