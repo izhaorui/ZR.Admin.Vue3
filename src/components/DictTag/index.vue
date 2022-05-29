ed44630586c64e4b8ebcc11eb461cbb8e64143a3
不做码农 <599854767@qq.com>
2022-05-29 14:19:50 +0800

    格式化页面

--- src/components/DictTag/index.vue
+++ src/components/DictTag/index.vue
@@ -1,16 +1,21 @@
 <template>
-  <div>
-    <template v-for="(item, index) in options">
-      <template v-if="values.includes(item.dictValue)">
-        <span v-if="item.listClass == 'default' || item.listClass == ''" :key="item.dictValue" :index="index" :class="item.cssClass">
-          {{ item.dictLabel }} <i v-if="showValue">#{{ item.dictValue }}</i></span
-        >
-        <el-tag size="small" v-else :disable-transitions="true" :index="index" :type="item.listClass == 'primary' ? '' : item.listClass" :class="item.cssClass">
-          {{ item.dictLabel }}<i v-if="showValue">#{{ item.dictValue }}</i>
-        </el-tag>
-      </template>
+  <template v-for="(item, index) in props.options">
+    <template v-if="values.includes(item.dictValue)">
+      <span v-if="item.listClass == 'default' || item.listClass == ''" :key="item.dictValue" :index="index" :class="item.cssClass">
+        {{ item.dictLabel }} <i v-if="showValue">#{{ item.dictValue }}</i>
+      </span>
+      <el-tag
+        size="small"
+        v-else
+        :disable-transitions="true"
+        :index="index"
+        :type="item.listClass == 'primary' ? '' : item.listClass"
+        :class="item.cssClass">
+        {{ item.dictLabel }}
+        <i v-if="showValue">#{{ item.dictValue }}</i>
+      </el-tag>
     </template>
-  </div>
+  </template>
 </template>
 
 <script setup>
