157119e2e06ef65cc8b18f98818e5eb66436358f
不做码农 <599854767@qq.com>
2022-06-05 14:46:34 +0800

    fix：面包屑样式显示异常

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -3,7 +3,9 @@
     <!-- <transition-group name="breadcrumb"> -->
     <el-breadcrumb-item v-for="(item, index) in levelList" :key="item.path">
       <span v-if="item.redirect === 'noRedirect' || index == levelList.length - 1" class="no-redirect">{{ item.meta.title }}</span>
-      <a v-else @click.prevent="handleLink(item)">{{ item.meta.title }}</a>
+      <span v-else @click.prevent="handleLink(item)" style="cursor: pointer">
+        {{ item.meta.title }}
+      </span>
     </el-breadcrumb-item>
     <!-- </transition-group> -->
   </el-breadcrumb>
