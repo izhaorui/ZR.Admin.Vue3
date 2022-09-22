f1a2c442465710da5d3f6f4b4a5580ee9cdcebc2
不做码农 <599854767@qq.com>
2022-09-22 09:23:37 +0800

    update dateOptions.js

--- src/utils/dateOptions.js
+++ src/utils/dateOptions.js
@@ -2,76 +2,76 @@
 import dayjs from 'dayjs'
 
 const dateOptions = [
-{
-  text: '昨天',
-  value: () => {
-    const end = new Date();
-    const start = new Date();
-    start.setTime(start.getTime() - 3600 * 1000 * 24);
-    end.setTime(end.getTime() - 3600 * 1000 * 24);
-    // console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'));
-    return [start, end]
-  }
-},
-{
-  text: '本周',
-  value: () => {
-    const end = dayjs().endOf('week').add(1, 'day').format('YYYY-MM-DD');
-    const start = dayjs().startOf('week').add(1, 'day').format('YYYY-MM-DD')
-    return [start, end]
-  }
-},
-{
-  text: '上周',
-  value: () => {
-    const start = dayjs().add(-1, 'week').startOf('week').add(1, 'day').format('YYYY-MM-DD')
-    const end = dayjs().add(-1, 'week').endOf('week').add(1, 'day').format('YYYY-MM-DD')
-    return [start, end]
-  }
-},
-{
-  text: '本月',
-  value: () => {
-    const end = dayjs().endOf('month').format('YYYY-MM-DD')
-    const start = dayjs().startOf('month').format('YYYY-MM-DD')
+  {
+    text: '昨天',
+    value: () => {
+      const end = dayjs().format('YYYY-MM-DD')
+      const start = dayjs().subtract(1, 'day').format('YYYY-MM-DD')
 
-    return [start, end]
+      // console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'));
+      return [start, end]
+    }
   },
-},
-{
-  text: '上月',
-  value: () => {
-    const end = dayjs().startOf('month').format('YYYY-MM-DD')
-    const start = dayjs().add(-1, 'month').startOf('month').format('YYYY-MM-DD')
+  {
+    text: '本周',
+    value: () => {
+      const end = dayjs().endOf('week').add(1, 'day').format('YYYY-MM-DD')
+      const start = dayjs().startOf('week').add(1, 'day').format('YYYY-MM-DD')
+      return [start, end]
+    }
+  },
+  {
+    text: '上周',
+    value: () => {
+      const start = dayjs().add(-1, 'week').startOf('week').add(1, 'day').format('YYYY-MM-DD')
+      const end = dayjs().add(-1, 'week').endOf('week').add(1, 'day').format('YYYY-MM-DD')
+      return [start, end]
+    }
+  },
+  {
+    text: '本月',
+    value: () => {
+      const end = dayjs().endOf('month').format('YYYY-MM-DD')
+      const start = dayjs().startOf('month').format('YYYY-MM-DD')
 
-    return [start, end]
+      return [start, end]
+    }
+  },
+  {
+    text: '上月',
+    value: () => {
+      const end = dayjs().startOf('month').format('YYYY-MM-DD')
+      const start = dayjs().add(-1, 'month').startOf('month').format('YYYY-MM-DD')
+
+      return [start, end]
+    }
+  },
+  {
+    text: '最近一周',
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+      return [start, end]
+    }
+  },
+  {
+    text: '最近一个月',
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+      return [start, end]
+    }
+  },
+  {
+    text: '最近三个月',
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+      return [start, end]
+    }
   }
-},
-{
-  text: '最近一周',
-  value: () => {
-    const end = new Date()
-    const start = new Date()
-    start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
-    return [start, end]
-  },
-},
-{
-  text: '最近一个月',
-  value: () => {
-    const end = new Date()
-    const start = new Date()
-    start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
-    return [start, end]
-  },
-},
-{
-  text: '最近三个月',
-  value: () => {
-    const end = new Date()
-    const start = new Date()
-    start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
-    return [start, end]
-  },
-}]
+]
 export default dateOptions
