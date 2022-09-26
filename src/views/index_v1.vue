e0a8a25b22981199f2eefba7622b1639cc450229
不做码农 <599854767@qq.com>
2022-09-26 21:08:25 +0800

    update index_v1.vue

--- src/views/index_v1.vue
+++ src/views/index_v1.vue
@@ -137,70 +137,70 @@ import useSocketStore from '@/store/modules/socket'
 const data = {
   newVisitis: {
     expectedData: [100, 120, 161, 134, 105, 160, 165],
-    actualData: [120, 82, 91, 154, 162, 140, 145],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
   },
   messages: {
     expectedData: [200, 192, 120, 144, 160, 130, 140],
-    actualData: [180, 160, 151, 106, 145, 150, 130],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
   },
   purchases: {
     expectedData: [80, 100, 121, 104, 105, 90, 100],
-    actualData: [120, 90, 100, 138, 142, 130, 130],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
   },
   shoppings: {
     expectedData: [130, 140, 141, 142, 145, 150, 160],
-    actualData: [120, 82, 91, 154, 162, 140, 130],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
   },
   wordCloud: [
     {
       name: 'Vue3',
-      value: 144,
+      value: 144
     },
     {
       name: '.net6',
-      value: 928,
+      value: 928
     },
     {
       name: 'Sqlsugar',
-      value: 906,
+      value: 906
     },
     {
       name: 'Redis',
-      value: 825,
+      value: 825
     },
     {
       name: '代码生成',
-      value: 514,
+      value: 514
     },
     {
       name: '开源免费',
-      value: 486,
+      value: 486
     },
     {
       name: '代码简单',
-      value: 53,
+      value: 53
     },
     {
       name: '打赏',
-      value: 163,
+      value: 163
     },
     {
       name: '国际化',
-      value: 163,
+      value: 163
     },
     {
       name: 'Composition api',
-      value: 1163,
+      value: 1163
     },
     {
       name: 'Quartz.Net',
-      value: 190,
+      value: 190
     },
     {
       name: 'Element UI',
-      value: 190,
-    },
-  ],
+      value: 190
+    }
+  ]
 }
 const { proxy } = getCurrentInstance()
 const userInfo = computed(() => {
@@ -227,7 +227,7 @@ function onChat(item) {
       confirmButtonText: '发送',
       cancelButtonText: '取消',
       inputPattern: /\S/,
-      inputErrorMessage: '消息内容不能为空',
+      inputErrorMessage: '消息内容不能为空'
     })
     .then(({ value }) => {
       proxy.signalr.SR.invoke('SendMessage', item.connnectionId, item.name, value).catch(function (err) {
