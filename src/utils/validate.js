ea8258ce5f37486a05e8c8e430507a1beea0b6ea
不做码农 <599854767@qq.com>
2022-09-29 21:47:42 +0800

    优化isHttp方法判断是否为null情况

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -4,7 +4,10 @@
  * @returns {Boolean}
  */
 export function isHttp(url) {
-  return url.indexOf('http://') !== -1 || url.indexOf('https://') !== -1
+  if (url) {
+    return url.indexOf('http://') !== -1 || url.indexOf('https://') !== -1
+  }
+  return false
 }
 
 /**
