e4115977c8b2e94193b5272049fee4467ff4bd55
不做码农 <599854767@qq.com>
2023-01-29 13:52:25 +0800

    fix：样式问题

--- src/views/register.vue
+++ src/views/register.vue
@@ -11,26 +11,21 @@
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input v-model="registerForm.password" type="password" auto-complete="off" placeholder="密码" @keyup.enter.native="handleRegister">
+        <el-input v-model="registerForm.password" type="password" auto-complete="off" placeholder="密码" @keyup.enter="handleRegister">
           <template #prefix>
             <svg-icon name="password" />
           </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="confirmPassword">
-        <el-input
-          v-model="registerForm.confirmPassword"
-          type="password"
-          auto-complete="off"
-          placeholder="确认密码"
-          @keyup.enter.native="handleRegister">
+        <el-input v-model="registerForm.confirmPassword" type="password" auto-complete="off" placeholder="确认密码" @keyup.enter="handleRegister">
           <template #prefix>
             <svg-icon name="password" />
           </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="code" v-if="captchaOnOff">
-        <el-input v-model="registerForm.code" auto-complete="off" placeholder="验证码" style="width: 63%" @keyup.enter.native="handleRegister">
+        <el-input v-model="registerForm.code" auto-complete="off" placeholder="验证码" style="width: 63%" @keyup.enter="handleRegister">
           <template #prefix>
             <svg-icon name="validCode" />
           </template>
@@ -40,7 +35,7 @@
         </div>
       </el-form-item>
       <el-form-item style="width: 100%">
-        <el-button :loading="loading" type="primary" size="large" style="width: 100%" @click.native.prevent="handleRegister">
+        <el-button :loading="loading" type="primary" size="large" style="width: 100%" @click.prevent="handleRegister">
           <span v-if="!loading">注 册</span>
           <span v-else>注 册 中...</span>
         </el-button>
@@ -69,7 +64,7 @@ const registerForm = reactive({
   password: '',
   confirmPassword: '',
   code: '',
-  uuid: '',
+  uuid: ''
 })
 
 const registerFormRef = ref(null)
@@ -89,8 +84,8 @@ const registerRules = reactive({
       min: 2,
       max: 20,
       message: '用户账号长度必须介于 2 和 20 之间',
-      trigger: 'blur',
-    },
+      trigger: 'blur'
+    }
   ],
   password: [
     { required: true, trigger: 'blur', message: '请输入您的密码' },
@@ -98,14 +93,14 @@ const registerRules = reactive({
       min: 5,
       max: 20,
       message: '用户密码长度必须介于 6 和 20 之间',
-      trigger: 'blur',
-    },
+      trigger: 'blur'
+    }
   ],
   confirmPassword: [
     { required: true, trigger: 'blur', message: '请再次输入您的密码' },
-    { required: true, validator: equalToPassword, trigger: 'blur' },
+    { required: true, validator: equalToPassword, trigger: 'blur' }
   ],
-  code: [{ required: true, trigger: 'change', message: '请输入验证码' }],
+  code: [{ required: true, trigger: 'change', message: '请输入验证码' }]
 })
 const copyRight = computed(() => {
   return defaultSettings.copyright
@@ -131,7 +126,7 @@ function handleRegister() {
             const username = registerForm.username
             ElMessageBox.alert("<font color='red'>恭喜你，您的账号 " + username + ' 注册成功！</font>', '系统提示', {
               dangerouslyUseHTMLString: true,
-              type: 'success',
+              type: 'success'
             })
               .then(() => {
                 router.push('/login')
@@ -163,12 +158,12 @@ getCode()
 .title {
   margin: 0px auto 30px auto;
   text-align: center;
-  color: #fff;
+  // color: #fff;
 }
 
 .register-form {
   border-radius: 6px;
-  background: rgba(255, 255, 255, 0.3);
+  background: #fff;
   width: 310px;
   padding: 25px 15px 5px 15px;
   .el-input {
