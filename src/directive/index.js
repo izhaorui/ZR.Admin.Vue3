44041620a10bea7d1591c5b29f234f2637b29531
不做码农 <599854767@qq.com>
2023-07-13 13:00:58 +0800

    :zap: 优化复制指令

--- src/directive/index.js
+++ src/directive/index.js
@@ -3,7 +3,6 @@ import hasPermi from './permission/hasPermi'
 import clipboard from './module/clipboard'
 import drag from './module/drag'
 import waves from './module/waves'
-// import copyText from './module/copyText'
 
 export default function directive(app) {
   app.directive('hasRole', hasRole)
@@ -11,5 +10,4 @@ export default function directive(app) {
   app.directive('clipboard', clipboard)
   app.directive('drag', drag)
   app.directive('waves', waves)
-  // app.directive('copyText', copyText)
 }
