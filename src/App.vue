f3558317983c54ed61e679d2ea52f9741ae3f915
不做码农 <599854767@qq.com>
2024-01-04 09:36:22 +0800

    :arrow_up:升级element-plus至2.4.4

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@
 import useUserStore from './store/modules/user'
 import useAppStore from './store/modules/app'
 import { ElConfigProvider } from 'element-plus'
-import zh from 'element-plus/dist/locale/zh-cn.mjs' // 中文语言
-import en from 'element-plus/dist/locale/en.mjs' // 英文语言
-import tw from 'element-plus/dist/locale/zh-tw.mjs' //繁体
+import zhCn from 'element-plus/dist/locale/zh-cn' // 中文语言
+import en from 'element-plus/dist/locale/en' // 英文语言
+import thTw from 'element-plus/dist/locale/zh-tw' //繁体
 import defaultSettings from '@/settings'
 const { proxy } = getCurrentInstance()
 
@@ -20,7 +20,7 @@ const token = computed(() => {
 const lang = computed(() => {
   return useAppStore().lang
 })
-const locale = ref(zh)
+const locale = ref(zhCn)
 const size = ref(defaultSettings.defaultSize)
 
 size.value = useAppStore().size
@@ -43,14 +43,12 @@ watch(
 watch(
   lang,
   (val) => {
-    if (val == 'zh-cn') {
-      locale.value = zh
-    } else if (val == 'en') {
+    if (val == 'en') {
       locale.value = en
     } else if (val == 'zh-tw') {
-      locale.value = tw
+      locale.value = thTw
     } else {
-      locale.value = zh
+      locale.value = zhCn
     }
   },
   {
