0423bd749a38466f0038d6974f7502a2b7891e4a
不做码农 <599854767@qq.com>
2024-06-12 18:35:19 +0800

    Fixes #IA4NZZ 修复vxe-table启动报错

--- src/vxe-tb.js
+++ src/vxe-tb.js
@@ -1,51 +1,65 @@
 // import XEUtils from 'xe-utils'
+import { VxeUI, VxeTable, VxeColumn } from 'vxe-table'
 import {
-  // 全局对象
-  // VXETable,
-  Icon,
-  Column,
-  // 表格
-  Tooltip,
-  Table
-} from 'vxe-table'
+  VxeButton,
+  VxeButtonGroup,
+  VxeCheckbox,
+  VxeCheckboxGroup,
+  VxeFormItem,
+  VxeForm,
+  VxeIcon,
+  VxeInput,
+  VxeModal,
+  VxePager,
+  VxePulldown,
+  VxeRadio,
+  VxeRadioButton,
+  VxeRadioGroup,
+  VxeSelect,
+  VxeSwitch,
+  VxeTextarea,
+  VxeTooltip,
+  VxeLoading
+} from 'vxe-pc-ui'
+// 导入主题变量，也可以重写主题变量
 import 'vxe-table/styles/cssvar.scss'
+import 'vxe-pc-ui/styles/cssvar.scss'
+// 导入默认的语言
+import zhCN from 'vxe-pc-ui/lib/language/zh-CN'
+
+VxeUI.setI18n('zh-CN', zhCN)
+VxeUI.setLanguage('zh-CN')
+
+function LazyVxeTable(app) {
+  app.use(VxeTable)
+  app.use(VxeColumn)
+  // app.use(VxeColgroup)
+  // app.use(VxeGrid)
+  // app.use(VxeToolbar)
+}
+
+function LazyVxeUI(app) {
+  // app.use(VxeButton)
+  // app.use(VxeButtonGroup)
+  // app.use(VxeCheckbox)
+  // app.use(VxeCheckboxGroup)
+  // app.use(VxeFormItem)
+  // app.use(VxeForm)
+  app.use(VxeIcon)
+  // app.use(VxeInput)
+  // app.use(VxeModal)
+  // app.use(VxePager)
+  // app.use(VxePulldown)
+  // app.use(VxeRadio)
+  // app.use(VxeRadioButton)
+  // app.use(VxeRadioGroup)
+  // app.use(VxeSelect)
+  // app.use(VxeSwitch)
+  // app.use(VxeTextarea)
+  app.use(VxeLoading)
+  app.use(VxeTooltip)
+}
 
 export default function useTable(app) {
-  // 表格功能
-  // app.use(Filter)
-  // .use(Edit)
-  // .use(Menu)
-  // .use(Export)
-  // .use(Keyboard)
-  // .use(Validator)
-
-  // 可选组件
-  app
-    .use(Column)
-    // .use(Colgroup)
-    // .use(Grid)
-    .use(Tooltip)
-    // .use(Toolbar)
-    // .use(Pager)
-    // .use(Form)
-    // .use(FormItem)
-    // .use(FormGather)
-    // .use(Checkbox)
-    // .use(CheckboxGroup)
-    // .use(Radio)
-    // .use(RadioGroup)
-    // .use(RadioButton)
-    // .use(Switch)
-    // .use(Input)
-    // .use(Select)
-    // .use(Optgroup)
-    // .use(Option)
-    // .use(Textarea)
-    // .use(Button)
-    // .use(Modal)
-    // .use(List)
-    // .use(Pulldown)
-
-    // 安装表格
-    .use(Table)
+  app.use(LazyVxeUI).use(LazyVxeTable)
 }
--- vite/plugins/index.js
+++ vite/plugins/index.js
@@ -4,7 +4,8 @@ import createAutoImport from './auto-import'
 import createSvgIcon from './svg-icon'
 import createCompression from './compression'
 import createSetupExtend from './setup-extend'
-import { createStyleImportPlugin, VxeTableResolve } from 'vite-plugin-style-import'
+// import { createStyleImportPlugin, VxeTableResolve } from 'vite-plugin-style-import'
+import { lazyImport, VxeResolver } from 'vite-plugin-lazy-import'
 
 export default function createVitePlugins(viteEnv, isBuild = false) {
   const vitePlugins = [vue()]
@@ -13,8 +14,24 @@ export default function createVitePlugins(viteEnv, isBuild = false) {
   vitePlugins.push(createSvgIcon(isBuild))
   isBuild && vitePlugins.push(...createCompression(viteEnv))
 
-  vitePlugins.push(createStyleImportPlugin({
-    resolves: [VxeTableResolve()]
-  }))
+  // vitePlugins.push(
+  //   createStyleImportPlugin({
+  //     resolves: [VxeTableResolve()]
+  //   })
+  // )
+
+  // Vxe 按需导入
+  vitePlugins.push(
+    lazyImport({
+      resolvers: [
+        VxeResolver({
+          libraryName: 'vxe-table'
+        }),
+        VxeResolver({
+          libraryName: 'vxe-pc-ui'
+        })
+      ]
+    })
+  )
   return vitePlugins
 }
