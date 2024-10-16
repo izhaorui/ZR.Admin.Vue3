36fc6ac1dae42d17f7b795d9e2cd34d707b551bd
不做码农 <599854767@qq.com>
2024-10-16 18:10:44 +0800

    :bug: fix组件语法警告

--- src/views/components/dictData.vue
+++ src/views/components/dictData.vue
@@ -103,7 +103,7 @@
         <el-col :lg="12">
           <el-form-item label="状态" prop="status">
             <el-radio-group v-model="form.status">
-              <el-radio v-for="dict in statusOptions" :key="dict.dictValue" :label="dict.dictValue">{{ dict.dictLabel }}</el-radio>
+              <el-radio v-for="dict in statusOptions" :key="dict.dictValue" :value="dict.dictValue" :label="dict.dictLabel"> </el-radio>
             </el-radio-group>
           </el-form-item>
         </el-col>
